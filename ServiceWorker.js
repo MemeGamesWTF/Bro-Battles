603dd15255e836f1831ad3a4b38c6d7bb02ad123
Sharanya7705 <110808482+Sharanya7705@users.noreply.github.com>
2025-06-04 16:44:52 +0530

    ui

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-DefaultGame-1.0";
 const contentToCache = [
-    "Build/Bro-Battles.loader.js",
-    "Build/Bro-Battles.framework.js",
-    "Build/Bro-Battles.data",
-    "Build/Bro-Battles.wasm",
+    "Build/Build.loader.js",
+    "Build/Build.framework.js",
+    "Build/Build.data",
+    "Build/Build.wasm",
     "TemplateData/style.css"
 
 ];
